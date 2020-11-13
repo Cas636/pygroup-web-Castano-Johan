39a47a13d46586da734c45ca0725b5b0186bdca2
Cas636 <castanojohan24@gmail.com>
2020-11-13 10:17:36 -0500

    Actualizacion

--- proyecto_flask/app/productos/views.py
+++
@@ -1,9 +0,0 @@
-  
-"""
-render_template de Flask:
-Flask utiliza por defecto jinja2 para generar documentos HTML, para generar una plantilla 
-utilizamos la función render_template que recibe como parámetro el fichero donde guardamos 
-la plantilla y las variables que se pasan a esta.
-Las plantillas las vamos a guardar en ficheros en el directorio templates (dentro del 
-directorio aplicacion).
-"""
